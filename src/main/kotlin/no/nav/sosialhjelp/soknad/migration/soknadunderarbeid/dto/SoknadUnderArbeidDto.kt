cce526841775d5cfcd68b3a34b4a8878e0318da2
martintveter <41987225+martintveter@users.noreply.github.com>
2022-04-07 13:27:04 +0200

    SoknadUnderArbeidService: dto -> soknadUnderArbeid før repo kalles (#14)

--- src/main/kotlin/no/nav/sosialhjelp/soknad/migration/soknadunderarbeid/dto/SoknadUnderArbeidDto.kt
+++ src/main/kotlin/no/nav/sosialhjelp/soknad/migration/soknadunderarbeid/dto/SoknadUnderArbeidDto.kt
@@ -2,6 +2,7 @@ package no.nav.sosialhjelp.soknad.migration.soknadunderarbeid.dto
 
 import no.nav.sbl.soknadsosialhjelp.soknad.JsonInternalSoknad
 import no.nav.sosialhjelp.soknad.migration.opplastetvedlegg.dto.OpplastetVedleggDto
+import no.nav.sosialhjelp.soknad.migration.soknadunderarbeid.domain.SoknadUnderArbeid
 import no.nav.sosialhjelp.soknad.migration.soknadunderarbeid.domain.SoknadUnderArbeidStatus
 import java.time.LocalDateTime
 
@@ -16,4 +17,19 @@ data class SoknadUnderArbeidDto(
     val opprettetDato: LocalDateTime,
     val sistEndretDato: LocalDateTime,
     val opplastetVedleggListe: List<OpplastetVedleggDto>
-)
+) {
+    fun toDomain(): SoknadUnderArbeid {
+        return SoknadUnderArbeid(
+            id = 0L, // dummy verdi
+            versjon = versjon,
+            behandlingsId = behandlingsId,
+            tilknyttetBehandlingsId = tilknyttetBehandlingsId,
+            eier = eier,
+            jsonInternalSoknad = jsonInternalSoknad,
+            status = status,
+            opprettetDato = opprettetDato,
+            sistEndretDato = sistEndretDato,
+            oldId = soknadId
+        )
+    }
+}
